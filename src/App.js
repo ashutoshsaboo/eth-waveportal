6acb75ef054f529e266dc38e4aae4e0820268902
Ashutosh Saboo <ashsaboo@amazon.com>
2021-10-18 17:08:50 +0530

    Add initial files

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import * as React from "react";
+import { ethers } from "ethers";
+import './App.css';
+
+export default function App() {
+
+  const wave = () => {
+    
+  }
+  
+  return (
+    <div className="mainContainer">
+
+      <div className="dataContainer">
+        <div className="header">
+        👋 Hey there!
+        </div>
+
+        <div className="bio">
+        Hi, this is my ethereum smart contract - dapp project! Connect your Ethereum wallet and wave at me!
+        </div>
+
+        <button className="waveButton" onClick={wave}>
+          Wave at Me
+        </button>
+      </div>
+    </div>
+  );
+}
